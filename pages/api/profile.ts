6e5a95c3277f71036c794c632337723ca207ce0d
MWolkowitsch <e1650767@student.tuwien.ac.at>
2022-01-29 16:35:55 +0100

    fix: fix data

--- pages/api/profile.ts
+++
@@ -1,8 +0,0 @@
-// pages/api/protected.js
-import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
-
-export default withApiAuthRequired(async function myApiRoute(req, res) {
-    const { user } = getSession(req, res)!;
-    res.json({ protected: 'My Secret', id: user.sub });
-});
-
